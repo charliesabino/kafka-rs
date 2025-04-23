a04a0316a5030953e00567480b77d2c4af187820
Charlie Sabino <94320712+charliesabino@users.noreply.github.com>
2025-04-22 19:57:55 -0500

    Implement request/response basics

--- src/broker.rs
+++ src/broker.rs
@@ -1,3 +1,5 @@
+use log::trace;
+
 use std::{
     io::{Read, Write},
     net::TcpListener,
@@ -16,17 +18,6 @@ pub struct ResponseMessage {
     response_header: ResponseHeader,
 }
 
-#[derive(Debug)]
-struct RequestHeader {
-    correlation_id: i32,
-}
-
-#[derive(Debug)]
-pub struct RequestMessage {
-    message_size: MessageSize,
-    response_header: RequestHeader,
-}
-
 impl ResponseMessage {
     fn new(message_size: MessageSize, response_header: ResponseHeader) -> Self {
         Self {
@@ -40,6 +31,51 @@ impl ResponseMessage {
         buf[..4].copy_from_slice(&self.message_size.to_be_bytes());
         buf[4..8].copy_from_slice(&self.response_header.correlation_id.to_be_bytes());
         buf
+    }
+}
+
+#[derive(Debug)]
+struct RequestHeader {
+    request_api_key: i16,
+    request_api_version: i16,
+    correlation_id: i32,
+    client_id: Option<String>,
+}
+
+impl RequestHeader {
+    fn from_bytes(bytes: &[u8]) -> Self {
+        let mut request_api_key_bytes = [0u8; 2];
+        let mut request_api_version_bytes = [0u8; 2];
+        let mut correlation_bytes = [0u8; 4];
+
+        request_api_key_bytes.copy_from_slice(&bytes[..2]);
+        request_api_version_bytes.copy_from_slice(&bytes[2..4]);
+        correlation_bytes.copy_from_slice(&bytes[4..8]);
+
+        Self {
+            request_api_key: i16::from_be_bytes(request_api_key_bytes),
+            request_api_version: i16::from_be_bytes(request_api_version_bytes),
+            correlation_id: i32::from_be_bytes(correlation_bytes),
+            client_id: None,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct RequestMessage {
+    message_size: MessageSize,
+    request_header: RequestHeader,
+}
+
+impl RequestMessage {
+    fn from_bytes(bytes: &[u8]) -> Self {
+        let mut size_bytes = [0u8; 4];
+        size_bytes.copy_from_slice(&bytes[0..4]);
+
+        Self {
+            message_size: i32::from_be_bytes(size_bytes),
+            request_header: RequestHeader::from_bytes(&bytes[4..]),
+        }
     }
 }
 
@@ -58,10 +94,38 @@ impl Broker {
         for stream in self.listener.incoming() {
             match stream {
                 Ok(mut _stream) => {
-                    let msg = ResponseMessage::new(0, ResponseHeader { correlation_id: 7 });
-                    _stream.write(&msg.to_bytes()).unwrap();
+                    let mut req = vec![0u8; 1024];
+                    let mut bytes_read = 0;
+
+                    trace!("Reading bytes");
+
+                    while bytes_read < std::mem::size_of::<MessageSize>() {
+                        bytes_read += _stream.read(&mut req[bytes_read..]).unwrap();
+                    }
+
+                    let mut message_size_bytes = [0u8; 4];
+                    message_size_bytes.copy_from_slice(&req[0..4]);
+                    let message_size = i32::from_be_bytes(message_size_bytes);
+
+                    trace!("Message size: {message_size}");
+
+                    while bytes_read < message_size as usize {
+                        bytes_read += _stream.read(&mut req[bytes_read..]).unwrap();
+                    }
+
+                    let req = RequestMessage::from_bytes(&req);
+                    trace!("Received request: {:?}", req);
+
+                    let resp_header = ResponseHeader {
+                        correlation_id: req.request_header.correlation_id,
+                    };
+
+                    let resp = ResponseMessage::new(0, resp_header);
+                    trace!("Sending response: {:?}", resp);
+
+                    _stream.write(&resp.to_bytes()).unwrap();
                 }
-                Err(e) => {}
+                Err(_e) => {}
             }
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,7 @@ use broker::Broker;
 use std::net::TcpListener;
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+    env_logger::init();
 
     let broker = Broker::new();
     broker.run();
